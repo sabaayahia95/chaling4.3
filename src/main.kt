26870bd47ed7a6525f6a18c598339ef1b4e25112
sabaa yahia <sabaa yahia4@gmail.com>
2020-09-05 12:59:22 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var list = listOf<Int>(8, 4, 7, 1, 2, 3, 0, 5, 6);
+
+    println(" sort the list ascending " + "   " + list.sorted());
+    println(" sort the list descending" + "    " + list.sortedDescending());
+
+
+}
